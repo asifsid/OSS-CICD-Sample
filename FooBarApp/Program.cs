282107cc0eeeb8f745d24df0f1d83a0f5d32bb14
Asif Siddiqi <asifsid@microsoft.com>
2018-09-12 11:31:07 -0700

    Fruites

--- FooBarApp/Program.cs
+++ FooBarApp/Program.cs
@@ -10,6 +10,7 @@ namespace FooBarApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Apples Oranges Bananas");
             Console.WriteLine("This line was added and modified.");
 
             Console.WriteLine("Hello World!");
